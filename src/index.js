bf564f9f60f37b50c7156214ce43ff9b549039ab
Senekalata <45321931+Senekalata@users.noreply.github.com>
2026-09-23 12:19:07 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ export default {
           "Access-Control-Allow-Origin": "*",
           "Access-Control-Allow-Methods": "GET, OPTIONS",
           "Access-Control-Allow-Headers": "Content-Type"
+          "cache-control": "public, max-age=300"
         }
       });
     }
